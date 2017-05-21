b9b6e6492fe565a5b66b28c776cee92b1ac0a1be
Pelusa753 <clau.arzg@gmail.com>
2017-05-21 17:36:58 +0200

    3.8 add description of change

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
   root 'static_pages#index'
+
   get 'static_pages/index'
+
+  #get 'static_pages/about'
+
+  #get 'static_pages/contact'
  
 	
 end
